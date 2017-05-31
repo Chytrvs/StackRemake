1f8fd8dfd0d0ca5d701de951cfb6d1acdac5ad23
Chytrvs <marcinchytrzynski@gmail.com>
2017-05-31 18:21:10 +0200

    Added Ui,Particle System

--- Assets/Scripts/StackController.cs
+++ Assets/Scripts/StackController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StackController : MonoBehaviour {
     public float FallSpeed=5f;
     public float TileSpeed = 5f;
+    public float TileScaleSpeed = 5f;
     public GameObject Tile;
     public GameObject HighestTile;
     public GameObject MovingTile;
@@ -15,12 +17,23 @@ public class StackController : MonoBehaviour {
     private float MaxTileScale;
     private GameObject Leftover;
     private float hue;
+    private int combo;
+    Vector3 DesiredScale;
+    private int Score = -1;
+    public Text ScoreTxt, HighScoreTxt;
+    public GameObject GameOverPanel;
+    public float ZoomOutSpeed;
+    bool IsGameOver;
+
+
+    
 	// Use this for initialization
 	void Start () {
         hue = Random.Range(0, 10) * 0.1f;
         Tile = transform.GetChild(transform.childCount-1).gameObject;
         MaxTileScale = Tile.transform.localScale.x;
         HighestTile = Tile;
+        DesiredScale = HighestTile.transform.localScale;
         SpawnNextTile();
         foreach(Transform tile in transform)
         {
@@ -31,16 +44,25 @@ public class StackController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //SetColor();
-        InputHandle();
-        MoveStackDown();
-        if (isMovingOnXaxis)
+        if (!IsGameOver)
         {
-            MoveTileX();
+            InputHandle();
+            MoveStackDown();
+            if (combo >= 3)
+                HighestTile.transform.localScale = Vector3.Lerp(HighestTile.transform.localScale, DesiredScale,  TileScaleSpeed * Time.deltaTime);
+            if (isMovingOnXaxis)
+            {
+                MoveTileX();
+            }
+            else
+            {
+                MoveTileZ();
+            }
         }
         else
-        {
-            MoveTileZ();
-        }
+            ZoomOutCamera();
+
+
 	}
     void InputHandle()
     {
@@ -49,7 +71,6 @@ public class StackController : MonoBehaviour {
             PlaceTile();
             SpawnNextTile();
         }
-;
     }
     void PlaceTile()
     {
@@ -61,11 +82,12 @@ public class StackController : MonoBehaviour {
                 MovingTile.transform.position = new Vector3(HighestTile.transform.position.x, MovingTile.transform.position.y, HighestTile.transform.position.z);
                 HighestTile = MovingTile;
                 isMovingOnXaxis = !isMovingOnXaxis;
+                HighestTile.GetComponent<ParticleSystem>().Emit(1000);
+                combo++;
             }
             else if (Displacement > HighestTile.transform.localScale.x)
             {
-                Debug.Log("lose");
-               
+                GameOver();
             }
             else
             {
@@ -83,6 +105,7 @@ public class StackController : MonoBehaviour {
                 MovingTile.transform.position = new Vector3((MovingTile.transform.position.x + HighestTile.transform.position.x) / 2, MovingTile.transform.position.y, HighestTile.transform.position.z);
                 HighestTile = MovingTile;
                 isMovingOnXaxis = !isMovingOnXaxis;
+                combo = 0;
 
             }
         }
@@ -94,10 +117,13 @@ public class StackController : MonoBehaviour {
                 MovingTile.transform.position = new Vector3(HighestTile.transform.position.x, MovingTile.transform.position.y, HighestTile.transform.position.z);
                 HighestTile = MovingTile;
                 isMovingOnXaxis = !isMovingOnXaxis;
+                HighestTile.GetComponent<ParticleSystem>().Emit(1000);
+                combo++;
             }
             else if (Displacement > HighestTile.transform.localScale.z)
             {
-                Debug.Log("lose");
+                GameOver();
+                combo = 0;
             }
             else
             {
@@ -115,21 +141,27 @@ public class StackController : MonoBehaviour {
                 MovingTile.transform.position = new Vector3(HighestTile.transform.position.x, MovingTile.transform.position.y,(MovingTile.transform.position.z + HighestTile.transform.position.z) / 2);
                 HighestTile = MovingTile;
                 isMovingOnXaxis = !isMovingOnXaxis;
+                combo = 0;
             }
         }
+        CheckCombo();
 
 
     }
     void SpawnNextTile()
     {
+        Score++;
+        ScoreTxt.text = Score.ToString();
         if (isMovingOnXaxis)
         {
         MovingTile = Instantiate(HighestTile,HighestTile.transform.position+new Vector3(-HighestTile.transform.localScale.x,1,0), Quaternion.identity, transform) as GameObject;
+            MovingTile.transform.localScale = DesiredScale;
             SetColor(MovingTile);
         }
         else
         {
         MovingTile = Instantiate(HighestTile, HighestTile.transform.position + new Vector3(0, 1, -HighestTile.transform.localScale.z), Quaternion.identity, transform) as GameObject;
+            MovingTile.transform.localScale = DesiredScale;
             SetColor(MovingTile);
         }
         EndPos += Vector3.down;
@@ -140,13 +172,13 @@ public class StackController : MonoBehaviour {
     }
     void MoveTileX() //Moves tile back and forth on X axis
     {
-        MovingTile.transform.position = new Vector3(HighestTile.transform.position.x + Mathf.PingPong(Time.time * TileSpeed, MaxTileScale * 2) - MaxTileScale, MovingTile.transform.position.y, MovingTile.transform.position.z);
+        MovingTile.transform.position = new Vector3(1.4f*(HighestTile.transform.position.x + Mathf.PingPong(Time.time * TileSpeed, MaxTileScale * 2) - MaxTileScale), MovingTile.transform.position.y, MovingTile.transform.position.z);
     }
     void MoveTileZ() //Moves tile back and forth on Z axis
     {
-        MovingTile.transform.position = new Vector3(MovingTile.transform.position.x, MovingTile.transform.position.y, HighestTile.transform.position.z + Mathf.PingPong(Time.time * TileSpeed, MaxTileScale*2) - MaxTileScale);
+        MovingTile.transform.position = new Vector3(MovingTile.transform.position.x, MovingTile.transform.position.y, 1.4f*(HighestTile.transform.position.z + Mathf.PingPong(Time.time * TileSpeed, MaxTileScale*2) - MaxTileScale));
     }
-    void SetColor(GameObject tile)
+    void SetColor(GameObject tile) //Sets higher hue for each tile to create gradient effect
     {
         if (hue < 1)
         {
@@ -156,10 +188,50 @@ public class StackController : MonoBehaviour {
         {
             hue = 0;
         }
-
-        
         float sat=0.6f;
         float val=0.6f;
-        tile.GetComponent<Renderer>().material.color =Color.HSVToRGB(hue, sat, val);
+        tile.GetComponent<Renderer>().material.color=Color.HSVToRGB(hue, sat, val);
+    }
+    void CheckCombo() //Checks if tile scale should be increased based on current combo
+    {
+        if (combo >= 3)
+        {
+            if(HighestTile.transform.localScale.x + 0.5f <=MaxTileScale||HighestTile.transform.localScale.x + 0.5f <=MaxTileScale)
+            DesiredScale = new Vector3(HighestTile.transform.localScale.x + 0.5f, 1, HighestTile.transform.localScale.z + 0.5f);
+            else
+            {
+                DesiredScale = new Vector3(MaxTileScale, 1, MaxTileScale);
+            }
+        }
+        else
+        {
+            DesiredScale = HighestTile.transform.localScale;
+        }
+    }
+    void GameOver()
+    {
+        if (!PlayerPrefs.HasKey("HighScore"))
+           PlayerPrefs.SetInt("HighScore", 0);
+        IsGameOver = true;
+        GameOverPanel.SetActive(true);
+        if (Score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", Score);
+            HighScoreTxt.text ="BEST "+ PlayerPrefs.GetInt("HighScore").ToString();
+        }
+        HighScoreTxt.text = "BEST " + PlayerPrefs.GetInt("HighScore").ToString();
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void PlayAgain()
+    {
+        Application.LoadLevel(0);
+        Time.timeScale = 1;
+    }
+    void ZoomOutCamera()
+    {
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, Score+ 7, ZoomOutSpeed*Time.deltaTime);
     }
 }
